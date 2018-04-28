1acb89515351d34cf8ff73d40383c541ef172df3
TudorFlorea <ftudorel92@yahoo.com>
2018-04-28 13:02:42 +0300

    inial commit

---
+++ models/product.js
@@ -0,0 +1,15 @@
+//Dependencies
+var restful = require('node-restful');
+var mongoose = require('mongoose');
+
+//Schema
+var productSchema = new mongoose.Schema(
+  {
+    name: String,
+    sku: String,
+    price: Number
+  }
+);
+
+//Return the model
+module.exports = restful.model('Products', productSchema);
